d690bfb75c013f3577707bb856042889d765b6e8
Ryan Guinter <421guinterr@gmail.com>
2022-07-09 15:05:25 -0600

    added random messages to random users

--- api/db/seeds/72_messages_mhp.js
+++ api/db/seeds/72_messages_mhp.js
@@ -2,6 +2,8 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
+ const { faker } = require('@faker-js/faker');
+
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('messages_mhp').del()
@@ -11,4 +13,22 @@ exports.seed = async function(knex) {
     {members_id_to: 53, members_id_from: 15, comment: 'Hello, Would you like to schedule an appointment?'},
     {members_id_to: 15, members_id_from: 53, comment: 'Thanks for reaching out, but maybe later.'},
   ]);
+  await knex('messages_mhp').insert(generateRandomMessages());
 };
+
+
+
+
+function generateRandomMessages() {
+  let result = []
+
+  for (let i = 0; i < 500; i++)
+  {
+    let randomFrom = Math.ceil(Math.random() * 10 + 10)
+    let randomTo = Math.ceil(Math.random() * 70 + 30)
+    let randomSentence = faker.lorem.sentences().substring(0,255);
+    result.push({members_id_to: randomTo, members_id_from: randomFrom, comment: randomSentence})
+  }
+
+  return result
+}
