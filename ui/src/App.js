d91c17aa8f31449a50d2bcb71003296915b59239
Zachary Haines <hainesz47@gmail.com>
2022-07-11 09:53:16 -0400

    removed initial boilerplate fetch for authors

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React from 'react';
 import config from './config'
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
@@ -6,23 +6,13 @@ import { useNavigate } from 'react-router-dom';
 const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 function App() {
-  const nav = useNavigate();
-  let [names, setNames] = useState([ ]);
   console.log(ApiUrl);
-
-  useEffect(() => {
-    fetch(ApiUrl + "/authors")
-      .then(response => response.json())
-      .then(data => setNames(data))
-      .catch(err => console.log(err))
-  }, []);
 
   return (
     <div>
       App is running - good work: 
       <Button onClick={()=>{nav('/login')}}>Login</Button>
       <Button onClick={()=>{nav('/signup')}}>Sign Up</Button>
-      { names.map(author => author.firstName + " ")}
     </div>
   );
 }
