5999b68483c62e9e3bd2f13f5fa765c59fe639f7
Zachary Haines <hainesz47@gmail.com>
2022-07-12 20:04:30 -0400

    resolved lint errors for unused values

--- api/db/seeds/71_survey_messages.js
+++ api/db/seeds/71_survey_messages.js
@@ -2,7 +2,6 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
-const { faker } = require('@faker-js/faker');
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
