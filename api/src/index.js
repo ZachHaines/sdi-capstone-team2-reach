81cd36611afd3ce43c6694e1c1be9aa25f8fa11d
Ryan Guinter <421guinterr@gmail.com>
2022-07-14 13:13:18 -0600

    added console log for port in api

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const PORT = process.env.PORT || 8082;
-
+console.log('Port here', PORT)
 app.listen(PORT, () => {
     console.log(`Capstone application listening on ${PORT}`);
 })
